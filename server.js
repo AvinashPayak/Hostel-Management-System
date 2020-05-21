55418824a524bf9cfb58338b62ebb6ea9c386a2c
AvinashPayak <avinashpayak@gmail.com>
2020-05-21 14:15:22 +0530

    backend and frontend setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const mainRoutes = require('./routes/main');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes.routes);
+
+
+app.listen(3000);
